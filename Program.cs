b657dac28f184a8f0334b1e52b4f656c34431590
Cezar <cezarlantosgmail.com>
2022-05-12 18:10:54 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace csharp_geometria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int value1, value2;
+                Console.WriteLine("Inserire il valore della base del rettangolo");
+                if (!int.TryParse(Console.ReadLine(), out value1))
+                {
+                    Console.WriteLine("Non hai inserito un numero");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine("Inserire il valore dell'altezza del rettangolo");
+                if (!int.TryParse(Console.ReadLine(), out value2))
+                {
+                    Console.WriteLine("Non hai inserito un numero");
+                    Environment.Exit(1);
+                }
+
+                Rettangolo Rettangolo = new Rettangolo(value1, value2);
+                Console.WriteLine("--RETTANGOLO 1--");
+                Console.WriteLine("Il perimetro del rettangolo è di {0}cm", Rettangolo.CalcolaPerimetro());
+                Console.WriteLine("L'area del rettangolo è di {0}m²", Rettangolo.CalcolaArea());
+            }
+
+            {
+                int value1, value2;
+                Console.WriteLine("Inserire il valore della base del rettangolo");
+                if (!int.TryParse(Console.ReadLine(), out value1))
+                {
+                    Console.WriteLine("Non hai inserito un numero");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine("Inserire il valore dell'altezza del rettangolo");
+                if (!int.TryParse(Console.ReadLine(), out value2))
+                {
+                    Console.WriteLine("Non hai inserito un numero");
+                    Environment.Exit(1);
+                }
+
+                Rettangolo Rettangolo = new Rettangolo(value1, value2);
+                Console.WriteLine("--RETTANGOLO 2--");
+                Console.WriteLine("Il perimetro del rettangolo è di {0}cm", Rettangolo.CalcolaPerimetro());
+                Console.WriteLine("L'area del rettangolo è di {0}m²", Rettangolo.CalcolaArea());
+            }
+
+            {
+                int value1, value2;
+                Console.WriteLine("Inserire il valore della base del rettangolo");
+                if (!int.TryParse(Console.ReadLine(), out value1))
+                {
+                    Console.WriteLine("Non hai inserito un numero");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine("Inserire il valore dell'altezza del rettangolo");
+                if (!int.TryParse(Console.ReadLine(), out value2))
+                {
+                    Console.WriteLine("Non hai inserito un numero");
+                    Environment.Exit(1);
+                }
+
+                Rettangolo Rettangolo = new Rettangolo(value1, value2);
+                Console.WriteLine("--RETTANGOLO 3--");
+                Console.WriteLine("Il perimetro del rettangolo è di {0}cm", Rettangolo.CalcolaPerimetro());
+                Console.WriteLine("L'area del rettangolo è di {0}m²", Rettangolo.CalcolaArea());
+            }
+        }
+    }
+}
---
+++ Rettangolo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace csharp_geometria
+{
+    internal class Rettangolo
+    {
+        private double iBaseRettangolo;
+        private double iAltezzaRettangolo;
+        public Rettangolo(double baseRet, double altezzaRet)
+        {
+            iBaseRettangolo = baseRet;
+            iAltezzaRettangolo = altezzaRet;
+        }
+        public double CalcolaPerimetro()
+        {
+            return (iBaseRettangolo + iAltezzaRettangolo) * 2;
+        }
+        public double CalcolaArea()
+        {
+            return iBaseRettangolo * iAltezzaRettangolo;
+        }
+    }
+}
